747d284e186c5d6c81deb49310decc403323561f
Radostin Handarov <radostin.handarov@gmail.com>
2025-12-26 18:04:05 +0200

    Add adding new event feature with all validations

--- backend/repositories/EventRepository.php
+++ backend/repositories/EventRepository.php
@@ -8,4 +8,30 @@ class EventRepository {
     public function __construct(\mysqli $db_connection) {
         $this->db_connection = $db_connection;
     }
+
+    public function add_event(
+        \models\Date $date,
+        \models\User $organizer,
+        string $location,
+        string $description
+    ): \models\Event {
+        $prepared_statement =
+            $this->db_connection->prepare(
+                "INSERT INTO events (date_id, organizer_id, `location`, `description`)
+                VALUES (?, ?, ?, ?)"
+            );
+
+        $date_id = $date->get_id();
+        $organizer_id = $organizer->get_id();
+        $prepared_statement->bind_param("iiss", $date_id, $organizer_id, $location, $description);
+        $prepared_statement->execute();
+
+        return new \models\Event(
+            $prepared_statement->insert_id,
+            $date,
+            $organizer,
+            $location,
+            $description
+        );
+    }
 }
--- backend/services/EventService.php
+++ backend/services/EventService.php
@@ -5,10 +5,96 @@ namespace services;
 class EventService {
     private \mysqli $db_connection;
     private \repositories\EventRepository $event_repository;
+    private \services\UserService $user_service;
 
-    public function __construct() {
+    public function __construct(\services\UserService $user_service) {
         $this->db_connection = \db\DBPool::get_instance()->get_connection();
         $this->event_repository = new \repositories\EventRepository($this->db_connection);
+        $this->user_service = $user_service;
+    }
+
+    public function add_event(
+        \models\Date $date,
+        \models\User $organizer,
+        string $location,
+        string $description,
+        array &$errors): ?\models\Event {
+        if ($this->is_organizer_the_same_as_celebrant($organizer, $date)) {
+            array_push($errors,
+                "Не може да организираш парти на себе си!");
+
+            return null;
+        }
+
+        if (!$this->validate_location($location, $errors)) {
+            return null;
+        }
+
+        if ($this->is_date_past($date)) {
+            array_push($errors,
+                "Датата на партито е минала!");
+
+            return null;
+        }
+
+        if (!$this->is_organizer_following_celebrant($organizer, $date)) {
+            array_push($errors,
+                "Организаторът не следва \"виновника\"!");
+
+            return null;
+        }
+
+        return $this->event_repository->add_event($date, $organizer, $location, $description);
+    }
+
+    private function is_organizer_the_same_as_celebrant(\models\User $organizer, \models\Date $date): bool {
+        return $organizer->get_id() === $date->get_owner()->get_id();
+    }
+
+    private function validate_location(string $location, array &$errors) : bool {
+        if ($location === "") {
+            array_push($errors,
+                "Локацията не бива да е празна!");
+
+            return false;
+        }
+
+        $location_length = mb_strlen($location);
+
+        if ($location_length > 256) {
+            array_push($errors,
+                "Дължината на локацията трябва да е не повече от 256 символа!");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private function is_date_past(\models\Date $date): bool {
+        return time() >= $this->get_actual_celebrating_date($date);
+    }
+
+    // Changes year of the date and returns it as a timestamp to compore woth
+    // the current moment.
+    private function get_actual_celebrating_date(\models\Date $date): int {
+        $day_and_month = date("m-d", strtotime($date->get_date()));
+        return strtotime(date("Y") . "-" . $day_and_month);
+    }
+
+    private function is_organizer_following_celebrant(
+        \models\User $organizer,
+        \models\Date $date
+    ): bool {
+        $organizer_followeds = $this->user_service->get_all_followers_of_user($organizer);
+
+        foreach ($organizer_followeds as $followed) {
+            if ($followed->get_id() === $date->get_owner()->get_id()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public function __destruct() {
