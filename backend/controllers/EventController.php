977a4abee01ec287046ab0cf8ef91a5df97aa670
Radostin Handarov <radostin.handarov@gmail.com>
2026-01-08 15:42:03 +0200

    Implement `GET /all-events` endpoint
    Add the auth checks, use services to obtain information
    about the user and all appropriate events and show
    the data to the user.

--- backend/controllers/EventController.php
+++ backend/controllers/EventController.php
@@ -15,4 +15,25 @@ class EventController {
         $this->user_service = $user_service;
         $this->event_service = $event_service;
     }
+
+    public function show_all_events(): void {
+        if (!SessionManager::is_logged_in()) {
+            header("Location: /login");
+            exit;
+        }
+
+        $user_id = SessionManager::get_logged_user_id();
+        $user = $this->user_service->find_user_by_id($user_id);
+
+        if ($user === null) {
+            header("Location: /login");
+            exit;
+        }
+
+        $all_events_for_this_user = $this->event_service->get_all_organizing_events_for_user($user);
+
+        include("templates/header.php");
+        include("templates/events/all-events.php");
+        include("templates/footer.php");
+    }
 }
---
+++ frontend/templates/events/all-events.php
@@ -0,0 +1,24 @@
+<main id="all-events">
+    <header>
+        <h2>Всички събития</h2>
+        <form method="GET" action="/my-dates/add/">
+            <button type="submit">Добави събитие</button>
+        </form>
+    </header>
+    <table class="table">
+        <tr>
+            <th scope="col">Заглавие</th>
+            <th scope="col">Място</th>
+            <th scope="col">Действия</th>
+        </tr>
+        <?php
+            foreach ($all_events_for_this_user as $event) {
+                echo "<tr>";
+                echo "<td>" . $event->get_title() . "</td>";
+                echo "<td>" . $event->get_location() . "</td>";
+                echo "<td><a href=\"#\">Детайли</a></td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</main>
