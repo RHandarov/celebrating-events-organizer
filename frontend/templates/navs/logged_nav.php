e0efa57143eb7a295f36edca90c42fb90e0fe0fe
Radostin Handarov <radostin.handarov@gmail.com>
2026-01-11 15:32:47 +0200

    Add my account link to the nav

--- frontend/templates/navs/logged_nav.php
+++ frontend/templates/navs/logged_nav.php
@@ -1,4 +1,7 @@
 <nav>
     <a href="/my-dates">Моите дати</a>
+    <?php
+        echo "<a href=\"/user/" . SessionManager::get_logged_user_id() . "\">Моят акаунт</a>";
+    ?>
     <a href="/logout">Излез</a>
 </nav>
