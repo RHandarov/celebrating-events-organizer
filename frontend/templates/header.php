506ee1ded6c0a49c843ad58ef92c54067ef9cd10
Radostin Handarov <radostin.handarov@gmail.com>
2026-01-18 14:02:08 +0200

    Remove bootstrap library
    Remove all Bootstrap files, reference in the header.
    After that the table and buttons' styles are changed
    to the alternative ones (with plain CSS). Also fix
    text fonts and page margin to be more aesthetic.

--- frontend/templates/header.php
+++ frontend/templates/header.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="UTF-8">
         <title>Организиране на събития</title>
-        <link type="text/css" rel="stylesheet" href="/css/bootstrap/css/bootstrap.min.css">
         <link type="text/css" rel="stylesheet" href="/css/style.css">
     </head>
     <body>
