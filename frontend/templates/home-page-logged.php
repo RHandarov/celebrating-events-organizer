a2a1b46434527cc94bef955882b526e47076dc28
Radostin Handarov <radostin.handarov@gmail.com>
2026-01-19 18:06:19 +0200

    Add dashboard to homepage when user is logged in
    The dashboard is split into two halves - 20 most recent
    events (left side) and all gifts of the user across all
    events in which the one is taking part.

---
+++ frontend/templates/home-page-logged.php
@@ -0,0 +1,40 @@
+<main id="home-page">
+    <div id="top-20-events">
+        <h2>20-те най-скорошни събития</h2>
+        <table class="table">
+            <tr>
+                <th scope="col">Заглавие</th>
+                <th scope="col">Дата</th>
+            </tr>
+            <?php
+                foreach ($top_20_events as $event) {
+                    echo "<tr>";
+                    echo "<td><a href=\"/event/" . $event->get_id() . "\">" . $event->get_title() . "</a></td>";
+                    echo "<td>" . $event->get_celebrating_date() . "</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+        <a href="/all-events">Виж всички събития</a>
+    </div>
+    <div id="buying-gifts">
+        <h2>Моите подаръци</h2>
+        <table class="table">
+            <tr>
+                <th scope="col">Описание</th>
+                <th scope="col">Събитие</th>
+                <th scope="col">Действия</th>
+            </tr>
+            <?php
+                foreach ($user_gifts as $gift) {
+                    echo "<tr>";
+                    echo "<td>" . $gift->get_description() . "</td>";
+                    echo "<td><a href=\"/event/" . $gift->get_event()->get_id() . "\">" . $gift->get_event()->get_title() . "</a></td>";
+                    $actions = "<a href=\"/gift/edit/" . $gift->get_id() . "\">Редактирай</a> | <a href=\"/gift/delete/" . $gift->get_id() . "?back=" . $event->get_id() . "\">Изтрий</a>";
+                    echo "<td>" . $actions . "</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </div>
+</main>
